67b32be366c2a15c51203d97086524fcfc1dd332
John Sturdy <jcg.sturdy@gmail.com>
2015-02-08 21:45:14 +0000

    Use string quotes where github is displaying colouring on adjacent brackets

--- languages/georgian.scm
+++ languages/georgian.scm
@@ -1,5 +1,5 @@
 (language-description (Georgian Kartvelian) 
-		      ([ქართული Kartuli] ქართველური)
+		      (["ქართული" Kartuli] "ქართველური")
 		      KAT
 		      ((mkhedruli alphabet
 				  "ა ბ გ დ ე ვ ზ თ ი კ ლ მ ნ ო პ ჟ რ ს ტ უ ფ ქ ღ ყ შ ჩ ც ძ წ ჭ ხ ჯ ჰ")
--- languages/korean.scm
+++ languages/korean.scm
@@ -1,5 +1,5 @@
 (language-description Korean
-		      [한국어 Hangugeo]
+		      ["한국어" Hangugeo]
 		      KOR
 		      ((hangul alphabet
 			       "ㄱ ㄲ ㄴ ㄷ ㄸ ㄹ ㅁ ㅂ ㅃ ㅅ ㅆ ㅇ ㅈ ㅉ ㅊ ㅋ ㅌ ㅍ ㅎ ㅏ ㅐ ㅑ ㅒ ㅓ ㅔ ㅕ ㅖ ㅗ ㅘ ㅙ ㅚ ㅛ ㅜ ㅝ ㅞ ㅟ ㅠ ㅡ ㅢ ㅣ")))
