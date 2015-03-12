d43423d43d01274b6f6d050c3b4685a1db14dddd
John Sturdy <jcg.sturdy@gmail.com>
2015-03-12 23:19:05 +0000

    Start writing some templates.

--- languages/german.scm
+++ languages/german.scm
@@ -3,3 +3,31 @@
 		      DEU
 		      ((latin alphabet
 			      "[aä] b c d e f g h i j k l m n [oö] p q r [sß] t [uü] v w x y z")))
+
+(template definite-article-for (noun noun-case noun-number)
+	  (case number
+	    ((plural) (case noun-case
+			((nominative accusative) "die")
+			((genitive) "der")
+			((dative) "den"))
+	     ((singular) (case (gender-of noun) 
+			   (masculine (case noun-case
+					((nominative)  "der")
+					((accusative) "den")
+					((genitive) "des")
+					((dative) "dem")))
+			   (feminine (case noun-case
+				       ((nominative accusative) "die")
+				       ((genitive dative) "der")))
+			   (neuter (case noun-case
+				     ((nominative accusative) "das")
+				     ((genitive)  "des")
+				     ((dative) "dem"))))))))
+
+(template noun-phrase (definite noun-case noun-number noun adjectives)
+	  (if definite
+	      (words (definite-article-for noun noun-case noun-number)
+		     adjectives
+		     noun)
+	      (words adjectives
+		     noun)))
