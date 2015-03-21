d8d8464e9efa53a130dae3bad5efba5e61ea1b29
John Sturdy <jcg.sturdy@gmail.com>
2015-03-21 22:26:05 +0000

    New file.

---
+++ languages/albanian.scm
@@ -0,0 +1,5 @@
+(language-description (Albanian Indo-European)
+		      Shqip
+		      ALN
+		      ((latin alphabet
+			      "A B C Ç D Dh E Ë F G Gj H I J K L Ll M N Nj O P Q R Rr S Sh T Th U V X Xh Y Z Zh")))
