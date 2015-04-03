2384dc87f923c4e2118e4b04e56c76304fa687dd
John Sturdy <jcg.sturdy@gmail.com>
2015-04-03 21:23:31 +0100

    Add the sorting order.

--- languages/russian.clj
+++ languages/russian.clj
@@ -1,4 +1,8 @@
 (language-description (Russian Slavonic Indo-European)
 		      ()
 		      RUS
-		      ((cyrillic alphabet)))
+		      ((cyrillic alphabet
+                                 ["а" "б" "в" "г" "д" "е" "ё" "ж" "з" "и"
+                                  "й" "к" "л" "м" "н" "о" "п" "р" "с" "т"
+                                  "у" "ф" "х" "ц" "ч" "ш" "щ" "ъ" "ы" "ь"
+                                  "э" "ю" "я"])))
