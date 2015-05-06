b2deded4ea1fcc844a83f7efb9e72bf68585ee0c
John Sturdy <jcg.sturdy@gmail.com>
2015-05-06 21:51:50 +0100

    Sort out defmacro syntax

--- src/lib/natlangfun.clj
+++ src/lib/natlangfun.clj
@@ -1,36 +1,38 @@
 ;;;; natlangfun.clj --- natural language descriptions using functional programming
-;;; Time-stamp: <2015-04-25 21:22:23 jcgs>
+;;; Time-stamp: <2015-05-06 21:51:36 jcgs>
 
 (ns natlangfun.core)
 
-(defmacro [language-description language-name own-name code scripts]
+(defmacro language-description
   "Describe a language which in the reference language is called
-@var{language-name}.
+  @var{language-name}.
 
-If @var{language-name} is a list, it describes the language's place in
-the tree of languages, with the specific language name coming first
-and increasingly broad language families following.
+  If @var{language-name} is a list, it describes the language's place in
+  the tree of languages, with the specific language name coming first
+  and increasingly broad language families following.
 
-The language's own name for itself is @var{own-name} (which, like the
-reference name, may be a list; it, or any components of it, may be
-arrays consisting of the word in the language's script and a
-transliteration into Latin script).
+  The language's own name for itself is @var{own-name} (which, like the
+  reference name, may be a list; it, or any components of it, may be
+  arrays consisting of the word in the language's script and a
+  transliteration into Latin script).
 
-If a name or name component will be read by Scheme as a symbol, it may
-be given as a Scheme symbol.  Otherwise, it should be given as a
-string.
+  If a name or name component will be read by Scheme as a symbol, it may
+  be given as a Scheme symbol.  Otherwise, it should be given as a
+  string.
 
-The language's ISO 639-3 code is given as @var{code}.
+  The language's ISO 639-3 code is given as @var{code}.
 
-The writing systems are described by @var{scripts}, which is a list of
-lists of script-type and script-name.  If a third element is given for
-a script description, it is a vector containing the characters used in
-that script, in their conventional sorting order for that language, as
-strings, with symbols that share a place in the sorting order grouped
-in square brackets in the same string.")
+  The writing systems are described by @var{scripts}, which is a list of
+  lists of script-type and script-name.  If a third element is given for
+  a script description, it is a vector containing the characters used in
+  that script, in their conventional sorting order for that language, as
+  strings, with symbols that share a place in the sorting order grouped
+  in square brackets in the same string."
+  [language-name own-name code scripts])
 
-(defmacro [template template-name template-parameters & templated-parts]
-  "Define a template for a language construct, to be filled in with the given parts.")
+(defmacro template
+  "Define a template for a language construct, to be filled in with the given parts."
+  [template-name template-parameters & templated-parts])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; word database lookups ;;
