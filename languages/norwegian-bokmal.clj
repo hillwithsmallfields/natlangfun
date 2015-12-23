96a53823ef1023fcbc5a98ebd186550b955b651c
John Sturdy <jcg.sturdy@gmail.com>
2015-12-23 20:57:16 +0000

    Add responses

--- languages/norwegian-bokmal.clj
+++ languages/norwegian-bokmal.clj
@@ -4,3 +4,9 @@
 		      ((latin alphabet
 			      ["a" "b" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m"
 			       "n" "o" "p" "r" "s" "t" "u" "v" "y" "æ" "ø" "å"])))
+(template response (reply expectation)
+          (case resp
+            affirmative (case expectation
+                          affirmative "ja"
+                          negative "jo")
+            negative "nei"))
